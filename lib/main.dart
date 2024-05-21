5a4d84742c13cb384dc1fb02985f614a268b15fe
Devam Kumar <devamkumar84@gmail.com>
2024-05-21 23:29:10 +0530

    Change title to backend branch

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'First Project',
+      title: 'First Project backend',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
